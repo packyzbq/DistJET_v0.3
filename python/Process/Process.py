8f46ee032c6f15b42a71906bfe6b6f0f72f802c9
techzbq <zbq_930713@163.com>
2017-09-15 16:53:37 +0800

    complete Process

--- python/Process/Process.py
+++ python/Process/Process.py
@@ -1,9 +1,12 @@
-import datetime
+import time
 import types
 import os,sys
+import threading
 import select
 import subprocess
-import python.Process.Parser
+import Queue
+import traceback
+from Parser import Parser
 
 
 class status:
@@ -19,9 +22,178 @@ class status:
         if status.DES.has_key(stat):
             return status.DES[stat]
 
+#FIXME start a sh process, cannot know when the command finished
+class Process_withENV:
+    """
+    start process with setup env,
+    """
+    def __init__(self,initial,logfile, shell=True, timeout=0, ignoreFaile=False, hook=None):
+        """
+        :param initial: the command of setup
+        :param logfile:  open file where exec_log write
+        :param shell:
+        :param timeout: select time out
+        :param ignoreFaile:
+        :param hook: when complete tasks, call this method
+        """
+        self.shell = shell
+        self.ignoreFail = ignoreFaile
+        self.exec_queue_lock = threading.RLock()
+        self.executable = Queue.Queue()
+
+        self.hook = hook
+        self.logFile = logfile
+        self.initial = []
+        if(type(initial) == types.ListType):
+            self.initial.extend(initial)
+        if(type(initial) == types.StringType):
+            self.initial.append(initial)
+
+        self.stdout = subprocess.PIPE
+        self.stdin = subprocess.PIPE
+        self.process = subprocess.Popen(['sh'], shell=self.shell, stdin=self.stdin, stdout=self.stdout, stderr=subprocess.STDOUT,preexec_fn=os.setsid())
+        self.pid = self.process.pid
+
+        self.logFile.write('@Process: create process for tasks, pid= %d\n' %self.pid)
+
+        self.timeout = timeout
+        self.recode = None
+        self.status = None
+
+        self.start = None
+        self.killed = None
+        self.fatalLine = None
+        self.logPharser = Parser()
+
+        self.ignoreFail = False
+        self.stop = False
+
+    def set_exe(self,command_list):
+        self.exec_queue_lock.acquire()
+        try:
+            for comm in command_list:
+                self.executable.put(comm)
+            self.executable.put('echo "recode:$?"')
+        finally:
+            self.exec_queue_lock.release()
+
+    def stop(self):
+        self.stop = True
+
+
+    def initialize(self):
+        """
+        setup env for process
+        :return: -1 no process; -2 setup timeout; 0 success;
+        """
+        if self.process is None:
+            return -1
+        if self.initial is None:
+            return 0
+        for comm in self.initial:
+            self.process.stdin.write(comm)
+        while True:
+            fs = select.select([self.process.stdout],[],[],10)
+            if not fs[0]:
+                self.logFile.write('@Process initial timeout')
+                return -2
+            if self.process.stdout in fs[0]:
+                data = os.read(self.process.stdout.fileno(), 1024)
+                if "recode" in data:
+                    ds = data.split()
+                    recode = ds[-1].split("recode")[-1][1:]
+                    return recode
+                else:
+                    self.logFile.write('[Setup_INFO] %s'%data)
+
+    def run(self):
+        while not self.stop:
+            try:
+                self.exec_queue_lock.acquire()
+                if not self.executable.empty():
+                    script = self.executable.get()
+                    self.exec_queue_lock.release()
+                    if script == 'exit':
+                        break
+                    self.start = time.time()
+                    self.logFile.write('\n' + '*' * 20 + ' script "%s" Running log ' % script[:-1] + '*' * 20 + '\n')
+                    self.process.stdin.write(script)
+                    while True:
+                        fs = select.select([self.process.stdout],[],[],self.timeout)
+                        if not fs[0]:
+                            # No response
+                            self.status = status.ANR
+                            if self.hook and callable(self.hook):
+                                self.hook(self.status,self.recode)
+                            self._kill_task()
+                            self.process = self._restart()
+                            break
+                        data = os.read(self.process.stdout.fileno(),1024)
+                        if not data:
+                            break
+                        st = data.split("\n")
+                        if "exitcode" in st[-2]:
+                            for line in st[:-2]:
+                                self.logFile.write(line)
+                            self.recode = st[-2][-1]
+                            self.logFile.write("return code = %s"%self.recode)
+                            if int(self.recode) == 0:
+                                self.status = status.SUCCESS
+                            if self.hook and callable(self.hook):
+                                self.hook(self.status, self.recode)
+                            break
+                        elif (not self.ignoreFail) and (not self._parseLog(data)):
+                            self.status = status.FAIL
+                            self.recode = -1
+                            if self.hook and callable(self.hook):
+                                self.hook(self.status, self.recode)
+                            self._kill_task()
+                            self.process = self._restart()
+                            break
+                        else:
+                            self.logFile.write(data)
+
+                else:
+                    self.exec_queue_lock.release()
+                    time.sleep(1)
+            except Exception,e:
+                self.logFile.write('@Process catch error: %s'%e.message)
+
+        self._burnProcess()
+
+
+
+    def _kill_task(self):
+        self.logFile.write('@Process: kill task\n')
+        if not self.process:
+            return
+        import os, signal
+        self.process.stdout.flush()
+        pgrp = os.getpgid(self.pid)
+        try:
+            os.killpg(pgrp,signal.SIGHUP)
+            self.process.wait()
+        except:
+            self.logFile.write()
+
+    def _restart(self):
+        proc = subprocess.Popen(['sh'], shell=self.shell, stdin=self.stdin, stdout=self.stdout, stderr=subprocess.STDOUT,preexec_fn=os.setsid())
+        self.pid = proc.pid
+
+        return proc
+
+    def _parseLog(self,data):
+        result,self.fatalline = self.logParser.parse(data)
+        return result
+
+    def _burnProcess(self):
+        self.process.terminate()
+        self.process.wait()
+
+
 
 class Process:
-    def __init__(self, exe, logfile, shell=True ,timeout=0, ignoreFail=False):
+    def __init__(self, exe, logfile, env_comm, shell=True,timeout=0, ignoreFail=False):
         self.shell = shell
         self.ignoreFail = ignoreFail
         self.executable = exe
@@ -44,7 +216,7 @@ class Process:
         self.fatalLine = None
 
     def run(self):
-        self.start = datetime.datetime.now()
+        self.start = time.time()
         self.logFile.write(self.executable)
         self.process = subprocess.Popen(args=self.executable, shell=self.shell, stdout=self.stdout,
                                         stderr=subprocess.STDOUT)
