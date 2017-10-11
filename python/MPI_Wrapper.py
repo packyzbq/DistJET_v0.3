1476955c706f5c010e8beed3e9af06c214a6baec
techzbq <zbq_930713@163.com>
2017-10-11 17:25:21 +0800

    add MPI_Wrapper and add workerAgent handle ack

---
+++ python/MPI_Wrapper.py
@@ -0,0 +1,99 @@
+import Server_Module as SM
+import Client_Module as CM
+#Waring remember to add exception handler: try catch
+MPI_log = None
+
+class Tags:
+    MPI_PING = 6
+
+    MPI_REGISTY = 1
+    MPI_REGISTY_ACK = 12
+    MPI_DISCONNECT = 2
+###### NUM <100 for mpich ; NUM>= 100 for python
+    WORKER_STOP     =100
+    WORKER_INFO     = 101   #w->m sync worker info (uuid, capacity)
+    WORKER_HALT     = 102
+
+    TASK_FIN        = 110      #w->m   worker notify completed tasks,
+
+    TASK_SYNC       = 111     #m<->w   master ask for work info
+                        # worker->master: wid,tid
+    TASK_ADD        = 112  # m->w   (tid, task_boot, task_data, res_dir)
+    TASK_REMOVE     = 113  # m->w   remove worker task, maybe give it to another worker (tid)
+
+    APP_INI         = 120   #m->w   master schedule app and transfer the init data  (app_ini_boot, app_ini_data, res_dir)
+                    #w->m   init result                                     (wid, res_dir)
+    APP_INI_ASK     = 121   #w->m ask for app ini boot and data
+    APP_FIN         = 122       #m->w   master tell worker how to finalize
+                        #W->M   worker ask for finalize operation
+    LOGOUT          = 130
+    LOGOUT_ACK      = 131    #m->w ack for worker logout requirement
+
+    NEW_APP         = 140   # master tell worker has a new app, and restart
+
+
+class Server:
+    """
+    Set up a server using C++ lib
+    """
+    def __init__(self, recv_buffer, svcname):
+        self.server = SM.MPI_Server(recv_buffer, svcname)
+        self.svcname = svcname
+    def initialize(self):
+        ret = self.server.initialize()
+        if ret != 0:
+            # log init error, boost return error need to be handled
+            MPI_log.error('[MPI server]: Initialize server error, error code=%d',ret)
+            return False
+        else:
+            MPI_log.info('[MPI server]: start server with service_name=%s', self.svcname)
+            return True
+
+    def send_int(self, int_data, msgsize, dest, tags):
+        self.server.send_int(int_data, msgsize, dest, tags)
+
+    def send_string(self, strs ,msgsize, dest, tag):
+        ret = self.server.send_string(strs, msgsize, str(dest), tag)
+        return ret
+
+    def run(self):
+        self.server.run()
+
+    def command_analyze(self, command):
+        pass
+
+    def stop(self):
+        ret = self.server.stop()
+        return ret
+
+class Client:
+    """
+    Set up a client(workerAgent) using C++ lib
+    """
+    def __init__(self, recv_buffer, svcname, uuid):
+        self.client = CM.MPI_Client(recv_buffer, svcname, uuid)
+        pass
+
+    def ping(self, uuid):
+        self.send_string(uuid, len(uuid), 0, Tags.MPI_PING)
+
+    def initial(self):
+        return self.client.initialize()
+
+    def run(self):
+        self.client.run()
+
+    def send_int(self, int_data, msgsize, dest, tags):
+        print('[Python-Client]: send string=%s, tag=%d' % (str, tags))
+        self.client.send_int(int_data, msgsize, dest, tags)
+
+    def send_string(self, strs ,msgsize, dest, tags):
+        return self.client.send_string(strs, msgsize, dest, tags)
+
+    def stop(self):
+        return self.client.stop()
+
+class MSG:
+    def __init__(self, tag, pack):
+        self.tag = tag
+        self.pack = pack
--- python/WorkerAgent.py
+++ python/WorkerAgent.py
@@ -179,8 +179,107 @@ class WorkerAgent:
         self.heartcond = threading.Condition()
         self.heartbeat = HeartbeatThread(self.client, self, self.heartcond)
 
-        self.worker_list = []
+        self.worker_list = {}
         self.worker_status={}
         self.cond_list = {}
 
     def run(self):
+        wlog.debug('[Agent] WorkerAgent run...')
+        self.heartbeat.start()
+        wlog.debug('[WorkerAgent] HeartBeat thread start...')
+        while not self.__should_stop:
+            time.sleep(0.1) #TODO temporary config for loop interval
+            if not self.recv_buff.empty():
+                msg = self.recv_buff.get()
+                if msg.tag == -1:
+                    continue
+                recv_dict = Package.unpack_obj(msg.sbuf)
+                for k,v in recv_dict.items():
+                    # registery info v={wid:val,init:[TaskObj], appid:v, wmp:worker_module_path}
+                    if int(k) == Tags.MPI_REGISTY_ACK:
+                        if v.has_key('flag') and v['flag'] == 'NEWAPP':
+                            wlog.debug('[WorkerAgent] Receive New App msg = %s' % v)
+                            v['wid'] = self.wid
+                            self.appid = v['appid']
+                            self.task_queue.queue.clear()
+                            self.task_completed_queue.queue.clear()
+                            self.ignoreTask = []
+                            self.tmpLock.acquire()
+                            try:
+                                self.initExecutor = None
+                                self.finExecutor = None
+                            finally:
+                                self.tmpLock.release()
+                            self.fin_flag = False
+                            self.app_fin_flag = False
+                            self.halt_flag = False
+                        else:
+                            wlog.debug('[WorkerAgent] Receive Registry_ACK msg = %s' % v)
+                        worker_path = v['wmp']
+                        if worker_path is not None and worker_path!='None':
+                            module_path = os.path.abspath(worker_path)
+                            sys.path.append(os.path.dirname(module_path))
+                            worker_name = os.path.basename(module_path)
+                            if worker_name.endswith('.py'):
+                                worker_name = worker_name[:-3]
+                            try:
+                                worker_module = __import__(worker_name)
+                                if worker_module.__dict__.has_key(worker_name) and callable(
+                                        worker_module.__dict__[worker_name]):
+                                    self.worker_class = worker_module.__dict__[worker_name]
+                                    wlog.info('[Agent] Load specific worker class = %s' % self.worker_class)
+                            except Exception:
+                                wlog.error('[Agent] Error when import worker module %s, path = %s,errmsg=%s' % (
+                                worker_name, worker_path, traceback.format_exc()))
+                        else:
+                            wlog.warning('[Agent] No specific worker input, use default')
+                        try:
+                            self.wid = v['wid']
+                            self.appid = v['appid']
+                            self.tmpLock.acquire()
+                            self.iniExecutor = v['init']
+                            self.tmpLock.release()
+
+                            # notify worker initialize
+                            wlog.info('[Agent] Start up worker and initialize')
+                            for i in range(self.capacity):
+                                self.cond_list.append(threading.Condition())
+                                self.worker_list[i]=Worker(i, self, self.cond_list[i], worker_class=self.worker_class)
+                                self.worker_status[i] = WorkerStatus.NEW
+                                wlog.debug('[Agent] Worker %s start' % i)
+                                self.worker_list[i].start()
+
+                            # notify the heartbeat thread
+                            wlog.debug('[WorkerAgent] Wake up the heartbeat thread')
+                            self.heartcond.acquire()
+                            self.heartcond.notify()
+                            self.heartcond.release()
+                        except Exception:
+                            pass
+                    # add tasks  v=[Task obj]
+                    elif int(k) == Tags.TASK_ADD:
+                        tasklist = v
+                        self.halt_flag = False
+                        wlog.debug('[WorkerAgent] Add new task : %s' % ([task.id for task in tasklist]))
+                        for task in tasklist:
+                            self.task_queue.put(task)
+                        count = len(tasklist)
+                        for worker_id, st in self.worker_status.keys():
+                            if st == WorkerStatus.IDLE:
+                                wlog.debug('[Agent] Worker %s IDLE, wake up worker' % worker_id)
+                                self.cond_list[worker_id].acquire()
+                                self.cond_list[worker_id].notify()
+                                self.cond_list[worker_id].release()
+                                count-=1
+                                if count == 0:
+                                    break
+                    # remove task, v=tid
+
+
+
+
+
+
+class Worker(BaseThread):
+    #TODO
+    pass
