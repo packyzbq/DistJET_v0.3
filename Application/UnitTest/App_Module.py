55c9cd3e3addb1eaad34cba9cc01632b5bdfd2ce
techzbq <zbq_930713@163.com>
2018-04-04 17:17:11 +0800

    update cmd

--- Application/UnitTest/App_Module.py
+++ Application/UnitTest/App_Module.py
@@ -1,13 +1,14 @@
 from python.IScheduler import SimpleTaskScheduler
+import os
 import UnitTestApp
 
 def run(app_config_path):
-    app = UnitTestApp.UnitTestApp("/afs/ihep.ac.cn/users/z/zhaobq/workerSpace/DistJET_v0.3/Application/UnitTest/",'UnitTest')
-    app.set_resdir("/afs/ihep.ac.cn/users/z/zhaobq/workerSpace/DistJET_v0.3/Application/UnitTest/res")
+    app = UnitTestApp.UnitTestApp(os.environ['DistJETPATH']+"/Application/UnitTest/",'UnitTest')
+    app.set_resdir(os.environ['DistJETPATH']+"/UnitTest")
     #app.set_resdir("/afs/ihep.ac.cn/users/z/zhaobq/workerSpace/DistJET_v0.3/UnitTest/res")
     app.set_scheduler(SimpleTaskScheduler)
-    app2 = UnitTestApp.UnitTestApp("/afs/ihep.ac.cn/users/z/zhaobq/workerSpace/DistJET_v0.3/Application/UnitTest/",'UnitTest')
-    app2.set_resdir("/afs/ihep.ac.cn/users/z/zhaobq/workerSpace/DistJET_v0.3/Application/UnitTest/res2")
-    app2.set_scheduler(SimpleTaskScheduler)
+    #app2 = UnitTestApp.UnitTestApp("/afs/ihep.ac.cn/users/z/zhaobq/workerSpace/DistJET_v0.3/Application/UnitTest/",'UnitTest')
+    #app2.set_resdir("/afs/ihep.ac.cn/users/z/zhaobq/workerSpace/DistJET_v0.3/Application/UnitTest/res2")
+    #app2.set_scheduler(SimpleTaskScheduler)
     return [app]
 
