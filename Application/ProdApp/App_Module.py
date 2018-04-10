a8b462f79ec62fd7da0e58b458653f3b8204dc06
techzbq <zbq_930713@163.com>
2018-04-10 14:30:46 +0800

    add test

--- Application/ProdApp/App_Module.py
+++ Application/ProdApp/App_Module.py
@@ -2,7 +2,7 @@ from python.IScheduler import SimpleTaskScheduler
 import ProdApp
 
 def run(app_config_path):
-    app = ProdApp.ProdApp("/junofs/users/zhaobq/DistJET_v0.3/Application/ProdApp/","ProdApp",config_path="config.ini")
+    app = ProdApp.ProdApp("/junofs/users/zhaobq/DistJET_v0.3/Application/ProdApp/","ProdApp",config_path=app_config_path)
     app.set_resdir("/junofs/users/zhaobq/DistJET_v0.3/ProdAppTest/res")
     app.set_scheduler(SimpleTaskScheduler)
     return [app]
