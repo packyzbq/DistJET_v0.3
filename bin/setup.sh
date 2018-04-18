bb745e94cac8e43f78aeb5c2c760443d4cf4d73a
techzbq <zbq_930713@163.com>
2018-04-18 14:58:15 +0800

    correct setup cmd

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,22 +4,26 @@ junopath="/afs/ihep.ac.cn/soft/juno/JUNO-ALL-SLC6/"
 
 if [ $# -le 1 ]
 then
-    junopath="$(junopath)/Pre-Release/J18v1r1-Pre1/"
+    path=${junopath}"/Pre-Release/J18v1r1-Pre1/"
 else
-    junopath=$(junopath)$1
+    path=${junopath}$1
+fi
 # setup JUNO software
 if [ -z $JUNOTOP ]
 then
-    source "$(junopath)/setup.sh"
+    source ${path}"/setup.sh"
 fi
 
-# setup mpi env
+echo "JUNO Software Environment Setup."
 
-export PATH=/junofs/users/zhaobq/mpich-install/bin:$PATH
+# setup mpi env
+export PATH="/junofs/users/zhaobq/mpi-install/bin:"$PATH
+echo "MPI Environment Setup."
 
 # setup python env
 export PYTHONPATH=/junofs/users/zhaobq/pylib/lib/python2.7/site-packages:$PYTHONPATH
 
 # setup DistJET env
 export DistJETPATH="/junofs/users/zhaobq/DistJET_v0.3"
-export DistJET_TMP="$(HOME)/.DistJET"
+export DistJET_TMP=${HOME}"/.DistJET"
+echo "DistJET Environment Setup."
